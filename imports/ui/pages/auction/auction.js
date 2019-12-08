4c33514516747492366e08a32e6c1327be9c3aa3
jk <jey.and.key@gmail.com>
2019-12-08 12:41:23 +0000

    commmit

--- imports/ui/pages/auction/auction.js
+++ imports/ui/pages/auction/auction.js
@@ -3,6 +3,7 @@ import './auction.html';
 import { Auctions, Bids, BidTypes } from '../../../api/cols.js'
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import dayjs from 'dayjs'
+import humanize from 'humanize-duration'
 
 Template.auction.onCreated(function() {
   const auReady = Meteor.subscribe('auctions.all');
@@ -13,16 +14,16 @@ Template.auction.onCreated(function() {
   this.autorun(() => {
     const id = FlowRouter.getParam("auctionId")
     const auction = Auctions.findOne(id)
-    if (auReady.ready() && biReady.ready()){
-      const bids = Bids.find({auctionId: id}, { sort: { index: -1 } }).fetch()
-      this.bidType.set(bids[0].index*1)
+    if (auReady.ready() && biReady.ready()) {
+      const bids = Bids.find({ auctionId: id }, { sort: { index: -1 } }).fetch()
+      this.bidType.set(bids[0].index * 1)
       console.log(bids[0])
     }
   })
-  this.autorun(()=>{
+  this.autorun(() => {
     console.log("state", this.bidType.get(), this.customBid.get(), BidTypes[this.customBid.get()])
   })
-  this.interval = setInterval(()=>{
+  this.interval = setInterval(() => {
     this.timeTicker.set('')
   }, 60)
 });
@@ -34,70 +35,70 @@ Template.auction.helpers({
     return res;
   },
   bids() {
-    let res = Bids.find({ show: { $ne: false } }, {sort: {amount: -1}})
+    let res = Bids.find({ show: { $ne: false } }, { sort: { amount: -1 } }).map(x=>{
+          const date = dayjs(x.date)
+          x.date = {date: date.format('D/M/YY'), time: date.format('hh:mm')}
+          console.log(x.date)
+          return x
+    })
     return res
   },
-  nextBid(){
-    return BidTypes[Template.instance().bidType.get()+1]
+  nextBid() {
+    return BidTypes[Template.instance().bidType.get() + 1]
   },
-  possibleBids(){
+  possibleBids() {
     const templ = Template.instance()
-    const {bidType, customBid} = templ
-    const res = BidTypes.map((x,i)=>{
-      return {amount: x, index: i}
-    }).slice(bidType.get()+1)
+    const { bidType, customBid } = templ
+    const res = BidTypes.map((x, i) => {
+      return { amount: x, index: i }
+    }).slice(bidType.get() + 1)
     customBid.set(res[0].index)
     $('.customBidSelect').val(0)
 
     return res
   },
-  selectFieldValue(){
-    return Template.instance().customBid.get()+""
+  selectFieldValue() {
+    return Template.instance().customBid.get() + ""
   },
-  whenEnds(){
+  whenEnds() {
     Template.instance().timeTicker.get()
-    console.log("time", dayjs(this.createdAt).add(3, 'day'))
     const end = dayjs(this.createdAt).add(3, 'day')
     const now = dayjs()
-    console.log("ticker",now.from(end))
-    return now.to(end)
+    if (now.isAfter(end)){
+      return "EXPIRED"
+    }
+    const diff = end - now
+    return "Ends In" + humanize(diff, { units: ['d', 'h', 'm'], round: true })
   }
 });
 
 Template.auction.events({
-  'change .customBidSelect'(event, templ){
+  'change .customBidSelect' (event, templ) {
     templ.customBid.set(event.target.value)
   },
-  'click .customBidMake'(event,templ){
+  'click .customBidMake' (event, templ) {
     const index = templ.customBid.get()
-        const amount = BidTypes[index]
-    console.log("custom", templ.customBid.get())
-    Meteor.call('bids.insert', 
-      { auctionId: FlowRouter.getParam("auctionId"),
-      index,
-      amount,
-    }, (err,res)=>{
-      if (err){
-        alert("Another user made the same bid before you")
-      } else {
-        $('.customBidSelect').val(0)
-      }
-    })
-  },
-  'click .bidNext' (event, templ) {
-    const index = templ.bidType.get()+1
     const amount = BidTypes[index]
-    console.log(index, templ.bidType.get())
-    Meteor.call('bids.insert', 
-      { auctionId: FlowRouter.getParam("auctionId"),
-      index,
-      amount,
+    const res = window.confirm("Would you like to bid for " + amount)
+    if (res) {
+      Meteor.call('bids.insert', {
+        auctionId: FlowRouter.getParam("auctionId"),
+        index,
+        amount,
+      }, (err, res) => {
+        if (err) {
+          alert("Another user made the same bid before you")
+        }
+        else {
+          $('.customBidSelect').val(0)
+        }
       })
-    console.log('bidding')
+    }
+
   },
 });
 
 
-Template.auction.onDestroyed(function(){
+Template.auction.onDestroyed(function() {
   clearInterval(this.interval)
 })

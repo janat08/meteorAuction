1d498d6514b24e6b5956c26747005b26ec6a559e
jk <jey.and.key@gmail.com>
2019-12-08 11:33:23 +0000

    commmit

--- imports/ui/pages/createAuction/createAuction.js
+++ imports/ui/pages/createAuction/createAuction.js
@@ -3,7 +3,7 @@ import './createAuction.html';
 import {Auctions, AuctionTypes} from '../../../api/cols.js'
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import dayjs from 'dayjs'
-
+window.dayjs = dayjs
 Template.createAuction.onCreated(function () {
   Meteor.subscribe('links.all');
 });
@@ -12,15 +12,26 @@ Template.createAuction.helpers({
   types() {
     return AuctionTypes;
   },
+  dateTime(){
+    const time = dayjs()
+    Template.instance().time = time
+    return {hour: time.format('HH'), date: time.format('DD MM YYYY'), minute: time.format('mm')}
+  }
 });
 
 Template.createAuction.events({
-  'submit'(event) {
+  'submit'(event, templ) {
     event.preventDefault();
 
     const target = event.target;
-    const {title: { value: tV },description: { value: dV }, type: { value: typeV }, minimum: { value: mV }} = target
-
+    const {title: { value: tV }, date: { value: dateV }, hour: { value: hourV }, minute: { value: minuteV}, description: { value: dV }, type: { value: typeV }, minimum: { value: mV }} = target
+    const date = dayjs(dateV)
+    console.log(date.toString(), dayjs(timeV).toString())
+    if (!date.isValid() || true){
+      console.log('invalid date')
+      return
+    }
+    if (templ.time )
     Meteor.call('auctions.insert', {title: tV, type: typeV, minimum: mV, description: dV}, (err, res)=>{
         console.log(res)
         if (err){

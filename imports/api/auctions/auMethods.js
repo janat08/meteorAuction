89beb6196c7683e3567dfa42b48c1f021b61a683
jk <jey.and.key@gmail.com>
2019-12-09 09:53:32 +0000

    commit

--- imports/api/auctions/auMethods.js
+++ imports/api/auctions/auMethods.js
@@ -2,10 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import { Auctions, AuctionTypes, BidTypeIndexes, BidTypes } from '../cols.js'
 import { Jobs } from 'meteor/msavin:sjobs'
 import dayjs from 'dayjs'
+
 Meteor.methods({
   'auctions.insert' ({ title, type: typeText, minimum, description, startDate }) {
     if (!this.userId) throw new Meteor.Error('not logged in')
-    const now = new Date()
     const type = typeText * 1
 
     if ([0, 1, 2].indexOf(type) == -1) throw new Meteor.Error("Error")
@@ -17,7 +17,7 @@ Meteor.methods({
       createdAt: new Date(),
       description,
     }
-    console.log(startDate)
+
     if (startDate) {
       Object.assign(document, {
         startDate: startDate,
@@ -28,19 +28,20 @@ Meteor.methods({
       document.active = true
     }
     const auctionId = Auctions.insert(document);
+    
     //make sure that biding start at the corresponding auction type
     if (type != 0) {
       const typeIndex = BidTypeIndexes[type] - 1
       const amount = BidTypes[typeIndex]
       Meteor.call('bids.insert', { index: typeIndex, auctionId, amount, show: false })
     }
+    
     if (startDate) {
       Jobs.run("activateAuction", auctionId, {
         date: dayjs(startDate).toDate()
       });
     }
-    const modifier = 3 * 24 * 60 * 60 * 1000
-    console.log('end', new Date(startDate), dayjs(startDate).toDate(), dayjs(startDate ? startDate : new Date()).add(3, 'day').toDate())
+
     Jobs.run("deactivateAuction", auctionId, {
       date: dayjs(startDate ? startDate : new Date()).add(3, 'day').toDate()
     });
--- imports/ui/pages/createAuction/createAuction.js
+++ imports/ui/pages/createAuction/createAuction.js
@@ -1,6 +1,6 @@
 import './createAuction.html';
 
-import { Auctions, AuctionTypes } from '../../../api/cols.js'
+import { Auctions, AuctionTypes, ImagesFiles } from '../../../api/cols.js'
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
@@ -13,7 +13,11 @@ dayjs.extend(customParseFormat)
 //gijgo freezes with recursive calls
 //flatpickr won't import styles, if styles imported manually nothing shows up
 Template.createAuction.onCreated(function() {
-
+    this.currentUpload = new ReactiveArray()
+    //meant to cause reactivity on object updates in current upload
+    this.insertedUploads = new ReactiveVar(0) 
+     //used to assign ids to files, so that there're unique ids between consequtive upload batches
+    this.numberOfRuns = 0
 });
 Template.createAuction.onRendered(function() {
 
@@ -22,6 +26,14 @@ Template.createAuction.onRendered(function() {
 Template.createAuction.helpers({
   types() {
     return AuctionTypes;
+  },
+  currentUpload() {
+    //meant for object reactivity
+    Template.instance().insertedUploads.get()
+    const curUpload = Template.instance().currentUpload.list()
+    const ids = curUpload.filter(x => x.doc).map(x => x.doc._id)
+    const query = [{ _id: { $in: ids } }]
+    return ImagesFiles.find({ $or: query }).each().concat(curUpload.filter(x => !x.doc));
   },
   dateTime() {
     const time = dayjs()
@@ -49,23 +61,73 @@ Template.createAuction.events({
       alert('invalid date, format (30 12 2019, hours 0-23 (15 is 3 pm)')
       return
     }
-    console.log(date)
-    var document = {title: tV, type: typeV, minimum: mV, description: dV }
-    
+    console.log(dayjs(date.unix()).toDate(), new Date(date.unix()))
+    var document = { title: tV, type: typeV, minimum: mV, description: dV }
+
     //if time is unchanged then don't set startDate
     if (!templ.time.isSame(date) && date.isAfter(dayjs())) {
-       document.startDate = date.unix()
+      document.startDate = date.toDate()
     }
-    
-      Meteor.call('auctions.insert', document , (err, res) => {
-        if (err) {
-          alert(err)
-        }
-        else {
-          return
-          FlowRouter.go('App.auction', { auctionId: res })
-        }
-      });
-    
+
+    Meteor.call('auctions.insert', document, (err, res) => {
+      if (err) {
+        alert(err)
+      }
+      else {
+        return
+        FlowRouter.go('App.auction', { auctionId: res })
+      }
+    });
+  },
+  'click .jsRemovePic' (e, templ) {
+    console.log(this)
+    Meteor.call('images.remove', this._id)
+    const st = templ.currentUpload
+    st.splice(st.findIndex(x => x.doc._id == this._id), 1)
+  },
+  'change #fileInput' (e, template) {
+    console.log(e.currentTarget.files)
+    const uploadPush = template.currentUpload.push
+    const st = template.currentUpload
+    const stRuns = template.numberOfRuns + ""
+    template.numberOfRuns += 1
+    window.ab = template.currentUpload
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      Array.from(e.currentTarget.files).forEach((x, i) => {
+        // We upload only one file, in case
+        // multiple files were selected
+        const upload = ImagesFiles.insert({
+          file: e.currentTarget.files[i],
+          streams: 'dynamic',
+          chunkSize: 'dynamic',
+          meta: {
+            uploader: Meteor.userId(),
+            productId: FlowRouter.getParam("productId"),
+          },
+        }, false);
+
+        const itemId = stRuns + i
+
+        upload.on('start', function() {
+          st.push({ upload: this, _id: itemId })
+        });
+
+        upload.on('end', function(error, fileObj) {
+          if (error) {
+            alert('Error during upload: ' + error);
+          }
+          else {
+            // alert('File "' + fileObj.name + '" successfully uploaded');
+          }
+          st[st.findIndex(x => x._id == itemId)].doc = fileObj
+          template.insertedUploads.set(stRuns + i)
+          // inter[i-1].doc = fileObj
+          // st.set(inter)
+        });
+
+        upload.start();
+      })
+
+    }
   },
 });

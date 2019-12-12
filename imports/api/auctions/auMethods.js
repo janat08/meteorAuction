f89fae3bcf0ed7f2e59a85be61cc78fbd8138ab4
jk <jey.and.key@gmail.com>
2019-12-12 10:53:29 +0000

    comm

--- imports/api/auctions/auMethods.js
+++ imports/api/auctions/auMethods.js
@@ -1,12 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import { Auctions, AuctionTypes, BidTypeIndexes, BidTypes, ImagesFiles } from '../cols.js'
-import { Jobs, JobsInternal } from 'meteor/msavin:sjobs'
+import { Auctions, Bids, AuctionTypes, BidTypeIndexes, BidTypes, ImagesFiles } from '../cols.js'
+// import { Jobs, JobsInternal } from 'meteor/msavin:sjobs'
+// import { agenda } from '../jobs/joMethods.js'
 import moment from 'moment'
 
-
+SyncedCron.start();
 
 Meteor.methods({
-  'auctions.insert' ({ imageIds, title, type: typeText, minimum, description, startDate }) {
+  'auctions.insert': async function({ imageIds, title, type: typeText, minimum, description, startDate }) {
     if (!this.userId) throw new Meteor.Error('not logged in')
     const type = typeText * 1
 
@@ -32,7 +33,7 @@ Meteor.methods({
       document.active = true
     }
     const auctionId = Auctions.insert(document);
-    ImagesFiles.update({ _id: { $in: imageIds } }, {$set:{ meta: { auctionId: auctionId }} })
+    ImagesFiles.update({ _id: { $in: imageIds } }, { $set: { meta: { auctionId: auctionId } } })
 
     //make sure that biding start at the corresponding auction type
     if (type != 0) {
@@ -40,16 +41,43 @@ Meteor.methods({
       const amount = BidTypes[typeIndex]
       Meteor.call('bids.insert', { index: typeIndex, auctionId, amount, show: false })
     }
-    
-    if (startDate) {
-      Jobs.run("activateAuction", auctionId, {
-        date: moment(startDate).toDate()
-      });
-    }
 
-    Jobs.run("deactivateAuction", auctionId, {
-      date: moment(startDate ? startDate : new Date()).add(3, 'day').toDate()
+    if (startDate) {
+      // Jobs.run("activateAuction", auctionId, {
+      //   date: moment(startDate).toDate()
+      // });
+      SyncedCron.add({
+        name: 'activate auction',
+        schedule: function(parser) {
+          // parser is a later.parse object
+          return parser.recur().on(startDate).fullDate()
+        },
+        job: function() {
+          Auctions.update(auctionId, { $set: { active: true } })
+        }
+      });
+      // agenda.schedule(moment(startDate).toDate(), 'activateAuction', { id: auctionId })
+    }
+    // const res = await agenda.schedule(moment(startDate ? startDate : new Date()).add(1, 'minute').toDate(), 'deactivateAuction', {id: auctionId})
+    // const res = await agenda.schedule(new Date(), 'deactivateAuction', {id: auctionId})
+    // const res = await agenda.now('deactivateAuction', { id: auctionId })
+
+
+
+    SyncedCron.add({
+      name: 'deactivate auction',
+      schedule: function(parser) {
+        // parser is a later.parse object
+        return parser.recur().on(moment(startDate ? startDate : new Date()).add(1, 'minute').toDate()).fullDate() 
+      },
+      job: function() {
+        const winner = Bids.findOne({ auctionId, show: true }, { sort: { amount: -1 } })
+        Auctions.update(auctionId, { $set: { active: false, finished: true, winner: winner && winner.userId, winnerAmount: winner.amount } })
+      }
     });
+    // Jobs.run("deactivateAuction", auctionId, {
+    //   date: moment(startDate ? startDate : new Date()).add(3, 'day').toDate()
+    // });
 
     return auctionId
   },
@@ -57,4 +85,4 @@ Meteor.methods({
     Auctions.remove({})
   }
 })
-    console.log(Object.keys(JobsInternal), JobsInternal.Utilities.helpers.getJob('deactivateAuction'))
+// console.log(Object.keys(JobsInternal), JobsInternal.Utilities.helpers.getJob('deactivateAuction'))
--- imports/api/jobs/joMethods.js
+++ imports/api/jobs/joMethods.js
@@ -1,33 +1,52 @@
-//activate deactivateauction
-import { Jobs } from 'meteor/msavin:sjobs'
-import { Auctions, Bids } from '../cols.js'
-import moment from 'moment'
-Jobs.register({
-    "activateAuction": function (id) {
-        console.log(id, 'running activate')
-        Auctions.update(id, {$set: {active: true}})
-        this.success()
-        this.remove()
-    },
-    "deactivateAuction": function (id) {
-        console.log('running deactivate')
-        const auction = Auctions.findOne(id)
-        const winner = Bids.findOne({auctionId: id, show: true}, {sort: {amount: -1}})
-        Auctions.update(id, {$set: {active: false, finished: true, winner: winner && winner.userId, winnerAmount: winner.amount}})
-        this.success()
-        this.remove()
-    },
-     "sendReminder": function (to, message) {
-        var instance = this;
+// //activate deactivateauction
+// import { Jobs } from 'meteor/msavin:sjobs'
+// import { Auctions, Bids } from '../cols.js'
+// import Agenda from 'agenda'
 
-        console.log('running shite whenever')
+// var agenda = new Agenda({ db: { address: process.env.MONGO_URL } })
 
-instance.remove()
-    }
-});
+// agenda.define('activateAuction', async function(job) {
+//     const { id } = job.attrs.data;
+//     await Auctions.update(id, { $set: { active: true } })
+//     console.log('activating')
+// })
+// agenda.define('deactivateAuction', async function(job) {
+//     const { id } = job.attrs.data;
+//     const winner = await Bids.findOne({ auctionId: id, show: true }, { sort: { amount: -1 } })
+//     await Auctions.update(id, { $set: { active: false, finished: true, winner: winner && winner.userId, winnerAmount: winner.amount } })
+//     console.log('deactivating')
+// })
 
- agenda = new Agenda({db: {address: process.env.MONGO_URL}})
+// agenda.start()
+// agenda.on('ready', async function() {
+//   const a = await agenda.jobs({name: 'deactivateAuction'})
+// console.log('jobs', new Date(), a.map(x=>x.attrs.nextRunAt))
+// });
 
-Meteor.startup(()=>{
-    Jobs.run("sendReminder", "jony@apple.com", "The future is here!", {date: moment().add(4, 'minute').toDate()});
-})
+
+
+// // Jobs.register({
+// //     "activateAuction": function(id) {
+// //         console.log(id, 'running activate')
+// //         Auctions.update(id, { $set: { active: true } })
+// //         this.success()
+// //         this.remove()
+// //     },
+// //     "deactivateAuction": function(id) {
+
+// //         console.log('running deactivate')
+// //         const auction = Auctions.findOne(id)
+// //         const winner = Bids.findOne({ auctionId: id, show: true }, { sort: { amount: -1 } })
+// //         Auctions.update(id, { $set: { active: false, finished: true, winner: winner && winner.userId, winnerAmount: winner.amount } })
+// //         this.success()
+// //         this.remove()
+// //     },
+// //     "sendReminder": function(to, message) {
+// //         var instance = this;
+
+// //         console.log('running shite whenever')
+
+// //         instance.remove()
+// //     }
+// // });
+// export { agenda }
